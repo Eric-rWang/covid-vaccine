c010dd9ada60c31528e4d1f9b18040afd45d5b18
Eric Wang <ericrwang2011@gmail.com>
2021-05-03 01:20:47 -0500

    added estimate func

--- US_vaccine_data/test.py
+++ US_vaccine_data/test.py
@@ -2,6 +2,12 @@ import uuid, json, os, csv, datetime
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import matplotlib as mpl
+from numpy import arange
+from scipy.optimize import curve_fit
+from datetime import date
+
+def objective(x, a, b, c, d):
+	return a * x**3 + b * x**2 + c * x + d
 
 with open('us_vaccine_data.csv', 'r') as csv_in:
 	csv_file = csv.reader(csv_in, delimiter=',')
@@ -24,6 +30,7 @@ with open('us_vaccine_data.csv', 'r') as csv_in:
 
 	x_values = [datetime.datetime.strptime(d,"%Y-%m-%d").date() for d in dates]
 
+	'''
 	plt.plot(x_values, fully_vaccinated)
 	plt.xlabel("Date")
 	plt.ylabel("Fully Vaccinated")
@@ -33,4 +40,34 @@ with open('us_vaccine_data.csv', 'r') as csv_in:
 	plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=10))
 	plt.gca().xaxis.set_minor_locator(mdates.DayLocator())
 	plt.gcf().autofmt_xdate()
+	#plt.savefig('vaccinated_graph.png')
 	plt.show()
+	'''
+
+	x = list(range(len(dates)))
+	popt, _ = curve_fit(objective, x, fully_vaccinated)
+	a, b, c, d = popt
+	print('y = %.5f * x^3 + %.5f * x^2 + %.5f * x + %.5f' % (a, b, c, d))
+
+	plt.scatter(x,fully_vaccinated, s=1)
+	x_line = arange(min(x), max(x), 1)
+	y_line = objective(x_line, a, b, c, d)
+	plt.plot(x_line, y_line, '--', color='green')
+	plt.show()
+
+	date1 = dates[0].split('-')
+	date2 = dates[99].split('-')
+	f_date = date(int(date1[0]), int(date1[1]), int(date1[2]))
+	l_date = date(int(date2[0]), int(date2[1]), int(date2[2]))
+	delta = l_date - f_date
+	print(type(delta.days))
+
+	print(objective(delta.days, a, b, c, d))
+
+
+
+
+
+
+
+
